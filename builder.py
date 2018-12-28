f20d2bcb5b47966ef184bc334d7852a5c31ca893
Vasilis Vouvoutsis <v.vouvoutsis@mellongroup.com>
2018-12-28 10:37:48 +0200

    Refraction and file string information

--- builder.py
+++ builder.py
@@ -42,80 +42,89 @@ def ssca_2008_hash_function(filename):
     return hash_value
 
 
-def build_header(unknown0, fileSize, executableName, hash, unknown1, version: enum = WinVer.Win10):
+def build_header(unknown0, file_size, executable_name, hash, unknown1, version: enum = WinVer.Win10):
     signature = 'SCCA'
     # Parse the file header
     # 84 bytes
-    version = struct.pack('I',version.value)
+    version = struct.pack('I', version.value)
     signature = signature.encode()
-    unknown0 = struct.pack('I',17) # 15 or 17
-    fileSize = struct.pack('I',fileSize)
-    executableName = struct.pack('60s', executableName.encode('utf-16-le'))
+    unknown0 = struct.pack('I', 17)  # 15 or 17
+    file_size = struct.pack('I', file_size)
+    executable_name = struct.pack('60s', executable_name.encode('utf-16-le'))
     # TODO: Calculate HASH
-    # rawhash = hex(struct.unpack_from("I", infile.read(4))[0])
-    # hash = rawhash.lstrip("0x")
+    raw_hash = struct.pack("I", 1)
+    # raw_hash = hex(struct.unpack_from("I", infile.read(4))[0])
+    # hash = raw_hash.lstrip("0x")
 
     unknown1 = struct.pack('I', 0)
 
-    header = version + signature + unknown0 + fileSize + executableName + rawhash + unknown1
+    header = version + signature + unknown0 + file_size + executable_name + raw_hash + unknown1
 
     return header
 
 
-def BuildfileInformation30(metricsCount, traceChainsOffset, traceChainsCount, filenameStringsOffset,
-                           filenameStringsSize, volumesInformationOffset, volumesCount, volumesInformationSize,
-                           unknown0, lastRunTime):
+def build_file_information_30(metrics_count, trace_chains_offset, trace_chains_count, filename_strings_offset,
+                              filename_strings_size, volumes_information_offset, volumes_count,
+                              volumes_information_size, unknown0, last_run_time):
     # File Information
     # 224 bytes
-    metricsOffset = 304
-    metricsOffset = struct.pack('I', metricsOffset)
-    metricsCount = struct.pack('I', metricsCount)
-    traceChainsOffset = struct.pack('I', traceChainsOffset)
-    traceChainsCount = struct.pack('I', traceChainsCount)
-    filenameStringsOffset = struct.pack('I', filenameStringsOffset)
-    filenameStringsSize = struct.pack('I', filenameStringsSize)
-    volumesInformationOffset = struct.pack('I', volumesInformationOffset)
-    volumesCount = struct.pack('I', volumesCount)
-    volumesInformationSize = struct.pack('I', volumesInformationSize)
+    metrics_offset = 304
+    metrics_offset = struct.pack('I', metrics_offset)
+    metrics_count = struct.pack('I', metrics_count)
+    trace_chains_offset = struct.pack('I', trace_chains_offset)
+    trace_chains_count = struct.pack('I', trace_chains_count)
+    filename_strings_offset = struct.pack('I', filename_strings_offset)
+    filename_strings_size = struct.pack('I', filename_strings_size)
+    volumes_information_offset = struct.pack('I', volumes_information_offset)
+    volumes_count = struct.pack('I', volumes_count)
+    volumes_information_size = struct.pack('I', volumes_information_size)
     unknown0 = struct.pack('II', 19, 1)  # Empty values
-    runCount = len(lastRunTime)
+    run_count = len(last_run_time)
     tmp = b''
-    for i in lastRunTime:
+    for i in last_run_time:
         delta = datetime.datetime.fromtimestamp(i) - datetime.datetime(1601, 1, 1)
         tmp += struct.pack('Q', delta)
-    tmp += struct.pack('Q', 0)*(8 - runCount)
-    lastRunTime = tmp
+    tmp += struct.pack('Q', 0) * (8 - run_count)
+    last_run_time = tmp
     unknown1 = struct.pack('Q', 0) * 2
-    runCount = struct.pack('I', runCount)
+    run_count = struct.pack('I', run_count)
     unknown2 = struct.pack('I', 1)  # 1, 2, 7
     unknown3 = struct.pack('I', 0)  # 0, 3
     unknown4 = struct.pack('I', 0) * 22
 
-    return metricsOffset + metricsCount + traceChainsOffset + traceChainsCount + filenameStringsOffset\
-        + filenameStringsSize + volumesInformationOffset + volumesCount + volumesInformationSize + unknown0\
-        + lastRunTime + unknown1 + runCount + unknown2 + unknown3 + unknown4
+    return metrics_offset + metrics_count + trace_chains_offset + trace_chains_count + filename_strings_offset \
+        + filename_strings_size + volumes_information_offset + volumes_count + volumes_information_size + unknown0 \
+        + last_run_time + unknown1 + run_count + unknown2 + unknown3 + unknown4
 
 
-def BuildmetricsArray30(unknown0, unknown1, unknown2, filenameOffset, filenameLength, unknown3, mftRecordNumber,
-                   mftSeqNumber):
+def build_metrics_array30(unknown0, unknown1, unknown2, filename_offset, filename_length, unknown3, mft_record_number,
+                          mft_seq_number):
     # File Metrics Array
     # 32 bytes per array, not parsed in this script
     # infile.seek(self.metricsOffset)
     unknown0 = struct.pack('I', unknown0)
     unknown1 = struct.pack('I', unknown1)
     unknown2 = struct.pack('I', unknown2)
-    filenameOffset = struct.pack('I', filenameOffset)
-    filenameLength = struct.pack('I', filenameLength)
+    filename_offset = struct.pack('I', filename_offset)
+    filename_length = struct.pack('I', filename_length)
     unknown3 = struct.pack('I', unknown3)
-    mftRecordNumber = mftRecordNumber
-    mftSeqNumber = struct.pack('H', mftSeqNumber)
+    mft_record_number = mft_record_number
+    mft_seq_number = struct.pack('H', mft_seq_number)
 
-    return unknown0 + unknown1 + unknown2 + filenameOffset + filenameLength + unknown3 + mftRecordNumber + mftSeqNumber
+    return unknown0 + unknown1 + unknown2 + filename_offset + filename_length + unknown3 + mft_record_number \
+        + mft_seq_number
 
 
-def traceChainsArray30(total_block, uknown0, uknown1, uknown2):
+def build_trace_chains_array30(total_block, unknown0, unknown1, unknown2):
     # Trace Chains Array
     # Read though, not being parsed for information
     # 8 bytes
-    trace_chane = struct.pack('IBBH', total_block, uknown0, uknown1, uknown2)
+    trace_chane = struct.pack('IBBH', total_block, unknown0, unknown1, unknown2)
     return trace_chane
+
+
+def build_filename_strings(filename_strings: list):
+    tmp = b''
+    for i in filename_strings:
+        tmp += i.encode('utf-16-le')
+    pass
